c3b17ed10fa2372a3d330d2365f3c3cf79fbad5f
Irineu <irineo.rocha@gmail.com>
2025-08-18 23:18:03 -0300

    Modifica para entrada de multiplas pessoas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,30 @@
 package org.example;
 
 import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa("Gabriel", 25);
+        Scanner scanner = new Scanner(System.in);
+        List<Pessoa> pessoa = new ArrayList<>();
         Gson gson = new Gson();
+
+        //Pessoa pessoa = new Pessoa("Gabriel", 25);
+        while (true) {
+        System.out.print("Digite o nome (ou FIM para encerrar): ");
+        String nome = scanner.nextLine();
+        if(nome.equalsIgnoreCase(FIM)){
+            break;
+        }
+        System.out.print("Digite a idade: ");
+            int idade;
+        pessoas.add(new Pessoa(nome, idade))
+    }
+
+        System.out.println("Objeto(s) convertido(s) para JSON:");
         String json = gson.toJson(pessoa);
-        System.out.println("Objeto convertido para JSON:");
         System.out.println(json);
     }
 
