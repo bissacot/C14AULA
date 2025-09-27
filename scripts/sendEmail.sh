886b4f16cb29a2e4810e41786127835f1c21c757
bissacot <89883296+bissacot@users.noreply.github.com>
2025-09-27 20:01:15 -0300

    Update sendEmail.sh
    update sendEmail

--- scripts/sendEmail.sh
+++ scripts/sendEmail.sh
@@ -1,4 +1,15 @@
 sudo apt-get install mailutils
+#!/usr/bin/env bash
+
+set -e
+ 
+if [ -z "$EMAIL_RECIPIENT" ]; then
+
+  echo "ERROR: EMAIL_RECIPIENT is not set."
+
+  exit 1
+
+fi
  
 # Trivial email body
 BODY="Pipeline executada com sucesso!"
